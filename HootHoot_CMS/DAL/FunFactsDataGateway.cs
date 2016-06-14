b59d200b6da94a589716992cc4e2a221da5bad4c
YeongChai-07 <butchercai@gmail.com>
2016-06-14 23:09:06 +0800

    - Added a STUB Random Item Generator for the purpose of generating a random FunFacts whenever the FunFacts API was called to retrieve a random FunFacts.
    - Included some TEST code to the FunFactsDataGateway.cs and the FunFactsController.cs to verify whether the Random Item Generator works as desired or not.

--- HootHoot_CMS/DAL/FunFactsDataGateway.cs
+++ HootHoot_CMS/DAL/FunFactsDataGateway.cs
@@ -8,6 +8,13 @@ namespace HootHoot_CMS.DAL
 {
     public class FunFactsDataGateway:DataGateway<FunFacts>
     {
+        public FunFacts SelectRandomFunFact ()
+        {
+            RandomItem_Generator<FunFacts> random = new RandomItem_Generator<FunFacts>
+                (this.SelectAll().ToList());
+
+            return random.getRandomItem(1).First();
+        }
         public FunFacts SelectByStationID(int stationID)
         {
             return this.dbData
--- HootHoot_CMS/DAL/RandomItem_Generator.cs
+++ HootHoot_CMS/DAL/RandomItem_Generator.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+/*using System.Text;
+using System.Threading.Tasks;*/
 
 namespace HootHoot_CMS.DAL
 {
-    class RandomItem_Generator
+    public class RandomItem_Generator<T> where T : class
     {
+        private Random randNum_Generator = null;
+        private IList<T> inputList = null;
+
+        public RandomItem_Generator(IList<T> input)
+        {
+            randNum_Generator = new Random();
+            inputList = input;
+        }
+
+        public IList<T> getRandomItem(int resultSize)
+        {
+            int randNum = -1;
+            int[] usedNum = new int[resultSize];
+
+            IList<T> ahYeah = new List<T>();
+
+            for(byte initCount=0;initCount < resultSize;initCount++)
+            {
+                usedNum[initCount] = -1;
+            }
+
+
+            byte i = 0;
+
+            while(i < resultSize)
+            {
+                randNum = randNum_Generator.Next(inputList.Count);
+                if (!(usedNum.Contains(randNum)))
+                {
+                    usedNum[i] = randNum;
+                    ahYeah.Add(inputList[randNum]);
+                    i++;
+                }
+
+            }
+
+            return ahYeah;
+            
+
+        }
+
+
     }
 }
