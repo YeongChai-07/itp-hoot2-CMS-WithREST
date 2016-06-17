0edcf9306dfca62c7ce06febd811ca2d3dd1680a
YeongChai-07 <butchercai@gmail.com>
2016-06-18 03:49:45 +0800

    -UPDATED the Questions model with the basic data annotations (Displaying title, data validations). -CREATED the MVC Controller for the Questions model (QuestionsController). -CREATED the Question MVC Views.

---
+++ HootHoot_CMS/Controllers/View Controller/QuestionsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HootHoot_CMS.DAL;
+using HootHoot_CMS.Models;
+
+namespace HootHoot_CMS.Controllers.View_Controller
+{
+    public class QuestionsController : Controller
+    {
+        private HootHootDbContext db = new HootHootDbContext();
+
+        // GET: Questions
+        public ActionResult Index()
+        {
+            var questions = db.Questions.Include(q => q.station);
+            return View(questions.ToList());
+        }
+
+        // GET: Questions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Questions questions = db.Questions.Find(id);
+            if (questions == null)
+            {
+                return HttpNotFound();
+            }
+            return View(questions);
+        }
+
+        // GET: Questions/Create
+        public ActionResult Create()
+        {
+            ViewBag.station_id = new SelectList(db.Stations, "station_id", "station_name");
+            return View();
+        }
+
+        // POST: Questions/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Questions questions)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Questions.Add(questions);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.station_id = new SelectList(db.Stations, "station_id", "station_name", questions.station_id);
+            return View(questions);
+        }
+
+        // GET: Questions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Questions questions = db.Questions.Find(id);
+            if (questions == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.station_id = new SelectList(db.Stations, "station_id", "station_name", questions.station_id);
+            return View(questions);
+        }
+
+        // POST: Questions/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Questions questions)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(questions).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.station_id = new SelectList(db.Stations, "station_id", "station_name", questions.station_id);
+            return View(questions);
+        }
+
+        // GET: Questions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Questions questions = db.Questions.Find(id);
+            if (questions == null)
+            {
+                return HttpNotFound();
+            }
+            return View(questions);
+        }
+
+        // POST: Questions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Questions questions = db.Questions.Find(id);
+            db.Questions.Remove(questions);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
